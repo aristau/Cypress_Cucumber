5dd173926481dc8419b343d2d7b390730fd35772
Anna Ristau <annaristau@Annas-MacBook-Pro.local>
2026-02-11 18:22:34 -0500

    Adding config for env variables

--- cypress_cucumber/cypress.config.js
+++ cypress_cucumber/cypress.config.js
@@ -2,9 +2,6 @@ const { defineConfig } = require("cypress");
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
 const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
 const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
-const nodemailer = require("nodemailer");
-const { ImapFlow } = require("imapflow");
-const simpleParser = require('mailparser').simpleParser;
 
 async function setupNodeEvents(on, config){
   await preprocessor.addCucumberPreprocessorPlugin(on, config);
@@ -16,28 +13,8 @@ async function setupNodeEvents(on, config){
     })
   );
 
-  // on("task", {
-  //   getTestEmailAccount() {
-  //     return emailAccount;
-  //   },
-  //   async getLastEmail({user, pass}) {
-  //     const get_Email = await getLastEmail(user, pass)
-  //     return get_Email
-  //   },
-  //   async parseEmail({message}){
-  //     const parse_Email = await parseEmail(message)
-  //     return parse_Email
-  //   },
-  //   async convertPDFtoHTML(filePath){
-  //     const html = await convertPDFtoHTML(filePath)
-  //     return html
-  //   }
-  // });
-
-
   return config;
 }
-
 
 module.exports = defineConfig({
   e2e: {
@@ -47,4 +24,11 @@ module.exports = defineConfig({
     chromeWebSecurity: false,
     setupNodeEvents
   },
+  env: {
+    validUsername: "standard_user",
+    validPassword: "secret_sauce",
+    invalidUsername: "invalid_user",
+    invalidPassword: "invalid_password",
+    lockedOutUsername: "locked_out_user"
+  }
 });
